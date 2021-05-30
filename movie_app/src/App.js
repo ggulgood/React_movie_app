6c7368596bdbc85db52debe3bd55f79104175cde
ggulgood <ggulgood@gmail.com>
2021-05-31 00:53:54 +0900

    #5.0

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component{
 
@@ -13,8 +14,10 @@ class App extends React.Component{
       data: {
         data: { movies }
       }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount(){
     this.getMovies();
@@ -22,10 +25,14 @@ class App extends React.Component{
   render(){
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -33,9 +40,12 @@ class App extends React.Component{
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
               ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
